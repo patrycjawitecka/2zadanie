a2571dfa333fe83b4f734df230392911bc9dcacc
Patrycja Witecka <dronkak@hotmail.com>
2020-01-15 20:55:37 +0100

    Add project files.

---
+++ 2zadanie/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2zadanie
+{
+    class Class1
+    {
+        //stan (zmienne- pola)
+        List<float> ratings = new List<float>();
+
+
+        //zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+        public float CalculateAverage()
+        {   //obliczanie średniej
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            avg = sum / ratings.Count();
+            return avg;
+        }
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+        public float GiveCountRating()
+        {
+            return ratings.Count();
+        }
+
+        public float GiveSumRating()
+        {
+            return ratings.Sum();
+        }
+        public float GiveMedianRating()
+        {
+            float med = 0;
+            ratings.Sort();
+            if (ratings.Count % 2 == 0)
+            {
+                med = (float)(ratings[(ratings.Count / 2) - 1] + ratings[(ratings.Count / 2)]) / 2;
+            }
+            else
+            {
+                med = (float)ratings[(ratings.Count / 2)];
+            }
+
+            return med;
+        }
+      
+        //tutaj dupa się zaczyna
+          public float GiveRating()
+        {
+            for (int i = 0; i < ratings.Count; i++)
+            {
+               Console.WriteLine(ratings[i]);
+            }
+        }
+        
+           
+}
+
+        
+
+
---
+++ 2zadanie/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _2zadanie
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 calc = new Class1();
+          
+            for (; ; )
+            {
+                Console.WriteLine("Podaj liczby większe od 0");
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
+               
+                if (rating == 0)
+                {
+                    break;
+                }
+                if (result == true)
+                {
+                    calc.AddRating(rating);          
+                }
+               
+            }
+
+           
+            //dopisacwyświetlanie pbranych liczb, ich ilość
+            //wyświetlanie liczb: Console.WriteLine(result);
+            Console.WriteLine("Wprowadzone liczby to:" + calc.GiveRating());
+            Console.WriteLine("Ilość wprowadzonych liczb to:" + calc.GiveCountRating());
+            Console.WriteLine("średnia twoich liczb to:" + calc.CalculateAverage());
+            Console.WriteLine("najwyższa liczba to:" + calc.GiveMaxRating());
+            Console.WriteLine("najniższa liczba to:" + calc.GiveMinRating());
+            Console.WriteLine("mediana liczb to:" + calc.GiveMedianRating());
+            Console.WriteLine("suma wyświetlanych liczb to:" + calc.GiveSumRating());
+            Console.ReadKey();
+
+        }
+    }
+ }
